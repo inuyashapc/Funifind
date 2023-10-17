7d0461f9356a9dccfc8a7352eb348dc2b4b896ea
ThangNT <thangnthe163199@fpt.edu.vn>
2023-10-17 14:41:14 +0700

    load data trang list user

--- src/pages/admin/list-user/index.js
+++ src/pages/admin/list-user/index.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "@/components/admin/navbar/navbar";
 import LayoutAdmin from "@/layouts/layoutAdmin";
+
 export default function ListUser() {
+  const [listUser, setListUser] = useState([]);
+
+  useEffect(() => {
+    // Lấy dữ liệu từ local storage
+    const userData = JSON.parse(localStorage.getItem("user"));
+
+    // Trích xuất token từ userData
+    const token = userData ? userData.accessToken : null;
+
+    // Kiểm tra xem có token hay không
+    if (token) {
+      // Tạo một đối tượng chứa các thông tin cần đính kèm vào header của request
+      const headers = {
+        "x-access-token": token,
+      };
+
+      fetch("http://localhost:8080/users", {
+        method: "GET",
+        headers: headers,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setListUser(data);
+        })
+        .catch((error) => {
+          console.error("Call API error: " + error);
+        });
+    } else {
+      console.error("No token in local storage");
+    }
+  }, []);
+  console.log(listUser);
+
   return (
     <LayoutAdmin>
       <div className="card-body">
@@ -25,66 +59,60 @@ export default function ListUser() {
                 <th>Name</th>
                 <th>Email</th>
                 <th>Phone</th>
-                <th className="pl-5 width200">Address</th>
-                <th>Joined</th>
+                <th className="pl-5">Address</th>
+                <th>Active</th>
                 <th />
               </tr>
             </thead>
             <tbody id="customers">
-              <tr className="btn-reveal-trigger">
-                <td>
-                  <div className="custom-control custom-checkbox mx-2">
-                    <input
-                      type="checkbox"
-                      className="custom-control-input"
-                      id="checkbox1"
-                    />
-                    <label
-                      className="custom-control-label"
-                      htmlFor="checkbox1"
-                    />
-                  </div>
-                </td>
-                <td className="py-3">
-                  <a href="#">
-                    <div className="media d-flex align-items-center">
-                      <div className="media-body">
-                        <h5 className="mb-0 fs--1">Ricky Antony</h5>
+              {listUser.map((user, index) => (
+                <tr key={index} className="btn-reveal-trigger">
+                  <td>
+                    <div className="custom-control custom-checkbox mx-2">
+                      <input
+                        type="checkbox"
+                        className="custom-control-input"
+                        id={`checkbox${index}`}
+                      />
+                      <label
+                        className="custom-control-label"
+                        htmlFor={`checkbox${index}`}
+                      />
+                    </div>
+                  </td>
+                  <td className="py-3">
+                    <a href="#">
+                      <div className="media d-flex align-items-center">
+                        <div className="media-body">
+                          <h5 className="mb-0 fs--1">{user.name}</h5>
+                        </div>
+                      </div>
+                    </a>
+                  </td>
+                  <td className="py-2">
+                    <a href={`https://mail.google.com/mail/?view=cm&fs=1&to=${user.email}`}>{user.email}</a>
+                  </td>
+                  <td className="py-2">{user.phoneNumber}</td>
+                  <td className="py-2 pl-5 wspace-no">{user.address}</td>
+                  <td className="py-2">
+                    <input type="checkbox" defaultChecked={user.isActive} />
+                  </td>
+                  <td className="py-2 text-right">
+                    <div className="dropdown">
+                      <div className="dropdown-menu dropdown-menu-right border py-0">
+                        <div className="py-2">
+                          <a className="dropdown-item" href="#!">
+                            Edit
+                          </a>
+                          <a className="dropdown-item text-danger" href="#!">
+                            Delete
+                          </a>
+                        </div>
                       </div>
                     </div>
-                  </a>
-                </td>
-                <td className="py-2">
-                  <a href="mailto:ricky@example.com">
-                    info@example.com
-                  </a>
-                </td>
-                <td className="py-2">
-                  {" "}
-                  <a href="tel:2012001851">(201) 200-1851</a>
-                </td>
-                <td className="py-2 pl-5 wspace-no">
-                  2392 Main Avenue, Penasauka
-                </td>
-                <td className="py-2">30/03/2018</td>
-                <td className="py-2 text-right">
-                  <div className="dropdown">
-                    <div className="dropdown-menu dropdown-menu-right border py-0">
-                      <div className="py-2">
-                        <a className="dropdown-item" href="#!">
-                          Edit
-                        </a>
-                        <a
-                          className="dropdown-item text-danger"
-                          href="#!"
-                        >
-                          Delete
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
